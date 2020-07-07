c6693181aff7fcd47fc0f2a41d8c8bf92bdfcda8
Mailson Dennis <mailssondennis@gmail.com>
2020-07-06 22:28:44 -0300

    env: adicionando configs da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  marvelApi: {
+    baseUrl: 'http://gateway.marvel.com/v1/public',
+    apiKey: '51dc18c89a43e19c034f153e43aef574'
+
+  }
 };
