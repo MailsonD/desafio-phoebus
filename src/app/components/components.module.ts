a8f6bb6c973c75fa7552ef17caa1fa2f20e8b009
Mailson Dennis <mailssondennis@gmail.com>
2020-07-06 23:53:57 -0300

    view: topbar com busca e responsivo

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,8 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [ HeaderComponent ],
@@ -12,6 +14,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
+    MatBadgeModule
   ],
   exports: [
     HeaderComponent,
