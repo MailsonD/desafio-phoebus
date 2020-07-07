f2799295aab0df3221eb3a2501c464ca42edd5ea
Mailson Dennis <mailssondennis@gmail.com>
2020-07-07 11:01:09 -0300

    config: criando modulo e rotas para as paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesModule } from './pages/pages.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => PagesModule },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,13 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="main">
+      <app-header></app-header>
+      <router-outlet></router-outlet>
+      <app-footer></app-footer>
+    </div>
+  `,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit {
